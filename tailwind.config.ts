ae4d764508219b4a4f7f3181b8156edf6ab00069
prakhar2005dtu-tech <prakhar2005dtu@gmail.com>
2025-12-31 20:57:38 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // <--- THIS LINE IS CRITICAL
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -12,6 +13,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sans: ['Arial', 'Helvetica', 'sans-serif'],
+      }
     },
   },
   plugins: [],
