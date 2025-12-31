fbff650ed8fa0f33bf51978eeff756e463b9a9b0
prakhar2005dtu-tech <prakhar2005dtu@gmail.com>
2025-12-31 18:27:54 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { NewYearLoveApp } from "@/components/new-year-love-app"
+
+export default function Page() {
+  return (
+    <main className="min-h-screen">
+      <NewYearLoveApp />
+    </main>
+  )
+}
